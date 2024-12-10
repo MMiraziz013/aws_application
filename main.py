d95986be91e09c12ff2fcd8bcd02b0435c3c7dfe
Ubuntu <ubuntu@ip-172-31-91-84.ec2.internal>
2024-12-10 12:21:07 +0000

    Prototype Version

--- main.py
+++ main.py
@@ -28,6 +28,25 @@ def fetch_data():
     database.close()
 
     return render_template('table.html', table_data=table_data)
+@app.route('/sort_by_difficulty')
+def sort_by_difficulty():
+	database = connect_to_database()
+	cursor = database.cursor()
+	cursor.execute("SELECT id, course_name, time_c, days, instructor, difficulty_level FROM aws_table ORDER BY difficulty_level")
+	cursor_data = cursor.fetchall()
+	database.close()
+
+	return render_template('table.html', table_data=cursor_data)
+
+@app.route('/unsort')
+def unsort():
+    database = connect_to_database()
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM aws_table")
+    table_data = cursor.fetchall()
+    database.close()
+
+    return render_template('table.html', table_data=table_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
